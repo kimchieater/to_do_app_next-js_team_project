0ee6a5628e91b34da3908084a0acdac47f603dbb
kimchieater <jasonj19276@gmail.com>
2024-06-09 13:53:25 +1000

    Update layout.tsx

--- to-do-app/src/app/layout.tsx
+++ to-do-app/src/app/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
   return (
       <ClerkProvider>
       <html lang="en" >
+<<<<<<< Updated upstream
         <body className="h-screen mx-auto  md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:max-w-[1536px]">  
+=======
+        <body className="dh-full overflow-y-hidden mx-auto w-[1450px] bg-neutral-300 p-10 ">  
+>>>>>>> Stashed changes
         <div className="p-10 bg-neutral-200 rounded-xl shadow-lg shadow-neutral-700/3 max-h-[800px]">
           <Nav></Nav>
           {children}
